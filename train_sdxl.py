0f8b44639d3605f7d1e2d18d3ccee7ec95c97b14
recoilme <vadim-kulibaba@yandex.ru>
2023-08-15 18:12:03 +0300

    errors

--- train_sdxl.py
+++ train_sdxl.py
@@ -899,7 +899,7 @@ def main():
             text_encoder=accelerator.unwrap_model(text_encoder_one),
             text_encoder_2=accelerator.unwrap_model(text_encoder_two),
             unet=accelerator.unwrap_model(unet),
-            revision=args.revision,
+            #revision=args.revision,
             torch_dtype=weight_dtype,
         )
 
